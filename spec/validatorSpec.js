d1b8862a69fe4d705fc848ba9d4acc7939bcad30
Ben Warner <benjaminnwarner@gmail.com>
2018-01-25 17:26:35 -0500

    initial commit

---
+++ spec/validatorSpec.js
@@ -0,0 +1,6 @@
+describe("validator", function(){
+
+  	it("should add some numbers", function(){
+  		expect(adder(1,1)).toEqual(2);
+  	})
+})
---
+++ validator.js
@@ -0,0 +1,10 @@
+function validateCBPList(cbpList){
+	var validityResult = [];
+
+	cbpList.forEach(function(cbp){
+
+		validityResult.push("")
+	})
+
+	return validityResult;
+}
