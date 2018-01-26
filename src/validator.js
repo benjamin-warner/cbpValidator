dc4d71e55975d447493d27eb589e90cffe1af85f
Ben Warner <benjaminnwarner@gmail.com>
2018-01-25 19:25:57 -0500

    Removed debug logging

--- src/validator.js
+++ src/validator.js
@@ -1,33 +1,27 @@
 function validateCBPList(cbpList){
-	console.log(cbpList)
 	var stack = []
+
 	var firstSymbol = cbpList.charAt(0)
 	var lastSymbol = cbpList.charAt(cbpList.length-1)
 	if(isCloser(firstSymbol) || !isCloser(lastSymbol)){
 		return "NO"
 	}
-
-	console.log("pushing", firstSymbol)
 	stack.push(firstSymbol)
 	
 	for(var i = 1; i < cbpList.length; i++){
-		console.log("Stack state at ", i , stack)
 		if(stack.length == 0){
 			return "NO"
 		}
 		var nextSymbol = cbpList.charAt(i)
 		var stackTop = stack[stack.length - 1]
 		if(doesNextSymbolClosePair(stackTop, nextSymbol)){
-			console.log("pop", stackTop, nextSymbol)
 			stack.pop()
 		}
 		else if(!isNextInvalid(stackTop, nextSymbol)){
-			console.log("valid", stackTop, nextSymbol)
 			stack.push(cbpList.charAt(i));
 
 		}
 		else{
-			console.log("NO", stackTop, nextSymbol)
 			return "NO"
 		}
 
